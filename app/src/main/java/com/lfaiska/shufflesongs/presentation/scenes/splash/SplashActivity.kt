19192463d87e0f7ea7550d8b3748ca273a8bdd86
Lucas <lucas.faiskaa@gmail.com>
2019-03-14 03:06:40 -0300

    Initial commit And Splash Screen

---
+++ app/src/main/java/com/lfaiska/shufflesongs/presentation/scenes/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.lfaiska.shufflesongs.presentation.scenes.splash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.lfaiska.shufflesongs.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
